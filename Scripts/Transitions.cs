3a22ff016722535123304a7f9a3cceb84d0db625
Hypehex <59145781+HypehexWorks@users.noreply.github.com>
2020-06-05 14:23:06 -0400

    Add files via upload

---
+++ Scripts/Transitions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transitions : MonoBehaviour
+{
+	public float transitionTime = 2f;
+
+	public Animator anim;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			StartCoroutine(loadNextLevel());
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftAlt))
+		{
+			StartCoroutine(loadPreviousLevel());
+		}
+	}
+
+	private IEnumerator loadNextLevel()
+	{
+		anim.SetTrigger("end");
+
+		yield return new WaitForSeconds(transitionTime);
+
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	private IEnumerator loadPreviousLevel()
+	{
+		anim.SetTrigger("end");
+
+		yield return new WaitForSeconds(transitionTime);
+
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+	}
+}
